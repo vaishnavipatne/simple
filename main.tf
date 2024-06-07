ce20447d093d300eb942b5b2c637c018e3f61823
vaishnavipatne <167867839+vaishnavipatne@users.noreply.github.com>
2024-06-07 08:35:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "google"{
+    project = "qwiklabs-gcp-04-702665bbcfd8"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+}
